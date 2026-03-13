2d4e1a2da4ec5b5b8d80195bba04ae669d8b0892
kkjsf <simon.fauret@gmail.com>
2026-03-13 13:49:31 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
-const CACHE  = 'harmonic-atlas-v2';
+const CACHE  = 'harmonic-atlas-v3';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/icons/apple-touch-icon.png',
-  '/icons/favicon-32.png'
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png',
+  './icons/favicon-32.png'
 ];
 
 self.addEventListener('install', e => {
